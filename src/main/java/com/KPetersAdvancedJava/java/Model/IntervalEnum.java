f696c8f8363efc4342bd0ec210e0e33c03dc3a5b
delph0 <centaurpilates@gmail.com>
2019-03-08 16:33:54 -0500

    Added comments

--- src/main/java/com/KPetersAdvancedJava/java/Model/IntervalEnum.java
+++ src/main/java/com/KPetersAdvancedJava/java/Model/IntervalEnum.java
@@ -1,6 +1,20 @@
+/**
+ * *  The Stock Service program provides data and dependencies for a Stock Tracking program
+ * *
+ * * @author  Kinni Peters
+ * * @version 1.8
+ * * @since 2/6/2019
+ */
+
 package com.KPetersAdvancedJava.java.Model;
 
+
 public enum IntervalEnum {
+
+    /**
+     * Enums to use in calculating intervals for the stock service
+     * This includes a message variable and a count variable
+     */
 
     MINUTE("Quote every minute", 1440),
     HOUR("Quote every hour", 24),
@@ -9,13 +23,33 @@ public enum IntervalEnum {
     private String message;
     private int count;
 
-    IntervalEnum(String message, int count)  {
+    /**
+     * Constructor for IntervalEnum
+     * @param message
+     * @param count
+     */
+
+    IntervalEnum(String message, int count) {
         this.message = message;
         this.count = count;
     }
 
-    public int getCount()  {return count;}
+    /**
+     * getter for the interval count
+     * @return count
+     */
 
-    public String getMessage()  {return message;}
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * getter for the enum message
+     * @return message
+     */
+
+    public String getMessage() {
+        return message;
+    }
 }
 
